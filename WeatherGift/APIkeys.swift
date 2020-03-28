cdb0f683c57052215c9997e7d0a0a0cc9fdf71e8
Shane Barys <shanejbarys@gmail.com>
2020-03-28 14:36:12 -0500

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Shane Barys on 3/28/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyDhc6j08UsDtxgPFh3lNtI2_SmoAt5W3e8"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Shane Barys on 3/28/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
