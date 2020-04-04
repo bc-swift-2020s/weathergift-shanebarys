f82244351a61e4fd853e3fe98fcc7cb64ae9c2b6
Shane Barys <shanejbarys@gmail.com>
2020-04-04 18:51:12 -0500

    7.11

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -19,36 +19,4 @@ class WeatherLocation: Codable {
         self.longitude = longitude
     }
     
-    func getData() {
-        
-        let coordinates = "\(latitude),\(longitude)"
-        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkyKey)/\(coordinates)"
-        
-        print("We are accessing the url \(urlString)")
-        
-        //Create a URL
-        guard let url = URL(string: urlString) else {
-            print("ERROR: Could not create URL from \(urlString)")
-            return
-        }
-        
-        //Create session
-        let session = URLSession.shared
-        
-        //Get data with .dataTask method
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("ERROR: \(error.localizedDescription)")
-            }
-            //deal with the data
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print(":) \(json)")
-            } catch {
-                print("JSON ERROR: \(error.localizedDescription)")
-            }
-        }
-        
-        task.resume()
-    }
 }
