c98df991f760914f0c8a94efd59bad84ed902031
Shane Barys <shanejbarys@gmail.com>
2020-04-04 19:42:06 -0500

    7.12

---
+++ WeatherGift/WeatherDetail.swift
@@ -0,0 +1,72 @@
+//
+//  WeatherDetail.swift
+//  WeatherGift
+//
+//  Created by Shane Barys on 4/4/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDetail: WeatherLocation {
+    
+    struct Result: Codable {
+        var timezone: String
+        var currently: Currently
+        var daily: Daily
+    }
+    
+    struct Currently: Codable {
+        var temperature: Double
+    }
+    
+    struct Daily: Codable {
+        var summary: String
+        var icon: String
+    }
+    
+    var timezone = ""
+    var temperature = 0
+    var summary = ""
+    var dailyIcon = ""
+    
+    func getData(completed: @escaping () -> () ) {
+        
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkyKey)/\(coordinates)"
+        
+        print("We are accessing the url \(urlString)")
+        
+        //Create a URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR: Could not create URL from \(urlString)")
+            completed()
+            return
+        }
+        
+        //Create session
+        let session = URLSession.shared
+        
+        //Get data with .dataTask method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            //deal with the data
+            do {
+                let result = try JSONDecoder().decode(Result.self, from: data!)
+                print("\(result)")
+                print("The timezone for \(self.name) is \(result.timezone)")
+                self.timezone = result.timezone
+                self.temperature = Int(result.currently.temperature.rounded())
+                self.summary = result.daily.summary
+                self.dailyIcon = result.daily.icon
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+            completed()
+        }
+        
+        task.resume()
+    }
+}
